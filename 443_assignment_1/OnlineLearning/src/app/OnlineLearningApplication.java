9d65868f4ce3bd49833b293b0a0f4db7a9dec81e
kaan taha köken <e215206@metu.edu.tr>
2019-10-23 18:47:20 +0300

    First java has been started

---
+++ 443_assignment_1/OnlineLearning/src/app/OnlineLearningApplication.java
@@ -0,0 +1,10 @@
+package app;
+/**
+ * @author Kaan Taha Köken
+ * @see <a href="https://github.com/kaankoken">Github</a>
+ */
+public class OnlineLearningApplication {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
