b828e1fd8846ef07bc86dd54845ad3c9e7212d68
kaan taha köken <e215206@metu.edu.tr>
2019-11-02 19:14:49 +0200

    Authentication method added in order to check login information

--- 443_assignment_1/OnlineLearning/src/app/ErrorHandling.java
+++ 443_assignment_1/OnlineLearning/src/app/ErrorHandling.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.List;
 import app.Trainee;
 
 public class ErrorHandling {
@@ -23,5 +24,16 @@ public class ErrorHandling {
             }
         }
         return true;
+    }
+
+    public boolean authenticate(List<Trainee> input, String Pass, String Email) {
+        for(Trainee t: input) {
+            System.out.println(t.getEmail() + " " + t.getPassword());
+            if (t.getEmail().equals(Email) && t.getPassword().equals(Pass)) {
+                return true;
+            }
+        }
+ 
+        return false;
     }
 }
