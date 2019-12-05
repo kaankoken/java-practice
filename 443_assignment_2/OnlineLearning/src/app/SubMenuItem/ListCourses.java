649c1e8e55096c0f57ba5d4a2699433eece75dc2
kaankoken <e215206@metu.edu.tr>
2019-12-06 00:35:06 +0200

    ListCourse classes is implemented for action listener class

---
+++ 443_assignment_2/OnlineLearning/src/app/SubMenuItem/ListCourses.java
@@ -0,0 +1,46 @@
+package app.SubMenuItem;
+
+import app.Course;
+
+import java.awt.GridBagLayout;
+import java.awt.Frame;
+import javax.swing.*;
+
+import java.util.*;
+public class ListCourses extends JDialog {
+    private static final long serialVersionUID = 7315490148533742415L;
+    private JPanel panel;
+    private JTable table;
+    private JScrollPane scrollPane;
+    
+    public ListCourses(Frame p, List<Course> course) {
+        super(p, "List All Courses", true);
+        panel = new JPanel(new GridBagLayout());
+        test(p, course);
+    }
+
+    private void test(Frame p, List<Course> course) {
+        String[] columns = {"Course Name", "Duration", "Instructor"};
+        Object[][] data = new Object[course.size()][3];
+        
+        int i = 0;
+        for (Course c: course) {
+            data[i][0] = c.courseName;
+            data[i][1] = c.duration;
+            data[i][2] = c.instructor.getName();
+            i++; 
+        }
+        
+        table = new JTable(data, columns);
+        scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        panel.add(scrollPane);
+
+        getContentPane().add(panel);
+        pack();
+
+        setResizable(true);
+        setLocationRelativeTo(p);
+    }
+}
