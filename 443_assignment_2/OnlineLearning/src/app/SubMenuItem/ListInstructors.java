d01a7a745d54eac06a8a19ae5de503bc7c172fe2
kaankoken <e215206@metu.edu.tr>
2019-12-10 13:56:33 +0200

    All the instructors now listable

---
+++ 443_assignment_2/OnlineLearning/src/app/SubMenuItem/ListInstructors.java
@@ -0,0 +1,44 @@
+package app.SubMenuItem;
+import app.Instructor;
+
+import java.awt.GridBagLayout;
+import java.awt.Frame;
+import javax.swing.*;
+import java.util.*;
+
+public class ListInstructors extends JDialog {
+    private static final long serialVersionUID = 1L;
+    private JPanel panel;
+    private JTable table;
+    private JScrollPane scrollPane;
+
+    public ListInstructors(Frame p, List<Instructor> instructors) {
+        super(p, "List of Instructors", true);
+        panel = new JPanel(new GridBagLayout());
+        displayData(p, instructors);
+    }
+
+    private void displayData(Frame p, List<Instructor> instructors) {
+        String[] columns = {"ID", "Name"};
+        Object[][] data = new Object[instructors.size()][2];
+        
+        int i = 0;
+        for (Instructor c: instructors) {
+            data[i][0] = c.ID;
+            data[i][1] = c.getName();
+            i++; 
+        }
+        
+        table = new JTable(data, columns);
+        scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        panel.add(scrollPane);
+
+        getContentPane().add(panel);
+        pack();
+
+        setResizable(true);
+        setLocationRelativeTo(p);
+    }
+}
